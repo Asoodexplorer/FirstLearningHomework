16ba6cd7d669181bf09ab87f197758e94c099c78
Amit Sood <asood.amit@gmail.com>
2022-10-15 17:30:39 +0100

    First Commit Learning

---
+++ src/NewGitLearnPush.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class NewGitLearnPush
+
+    {
+
+    public static void main(String[] args)
+
+    {
+
+        int a,b;
+        double add;
+        Scanner scanner =new Scanner(System.in);
+
+        System.out.println( "Enter First Number");
+
+        a = scanner.nextInt();
+
+        System.out.println( "Enter Second Number");
+        b = scanner.nextInt();
+
+        add = a + b;
+        System.out.println( "Addition of a + b = " + add);
+    }
+
+
+
+
+
+
+
+}
